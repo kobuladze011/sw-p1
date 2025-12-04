1bc62757cacb79b0529685d254b15076df1c5b45
Barbare Gelashvili <161972787+barbare2225@users.noreply.github.com>
2025-12-04 14:30:48 +0400

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { CurrencyProvider } from "./context/CurrencyContext";
-
-ReactDOM.render(
-  <CurrencyProvider>
-    <App />
-  </CurrencyProvider>,
-  document.getElementById("root")
-);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
